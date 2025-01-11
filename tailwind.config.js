fab907c8a37baba05f73b9b4ad6c31f3618318e8
Yossia Dwi Mahardika <yossia.dwi@ti.ukdw.ac.id>
2025-01-11 13:36:20 +0700

    fix tailwind issue

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 
-const flowbite = require("flowbite-react/tailwind");
+import flowbite from "flowbite-react/tailwind";
 
 export default {
   content: [
